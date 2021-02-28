8070a6cfae1437e0b56a1a43d624d942d42244fd
Bea <beatriztattooer@gmail.com>
2021-02-28 14:11:53 -0300

    T-SHIRT AND ARMS :)

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,10 @@ function blink(){
     var eyeLeft = document.getElementsByClassName('eyes--left');
     var eyeRight = document.getElementsByClassName('eyes--right');
 
+    var trunk = document.getElementsByClassName('trunk');
+    var armsLeft = document.getElementsByClassName('arms--left');
+    var armsRight = document.getElementsByClassName('arms--right');
+
 
     head[0].classList.add('skin--blink');
     forehead[0].classList.add('skin--blink');
@@ -17,4 +21,9 @@ function blink(){
 
     eyeLeft[0].classList.add('eyes--blink');
     eyeRight[0].classList.add('eyes--blink');
+
+    trunk[0].classList.add('trunk--blink');
+    armsLeft[0].classList.add('arms--left--blink');
+    armsRight[0].classList.add('arms--right--blink');
+    
 }
