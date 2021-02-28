06b3e1f8d328c97a8bc0c5d8d0343d9b5a942dd3
Bea <beatriztattooer@gmail.com>
2021-02-28 14:32:14 -0300

    THE BLINK GUY (RUDE MOOD) IS DONE ;)

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 function blink(){
+    var hair = document.getElementsByClassName('hair');
+
     var head = document.getElementsByClassName('head');
     var forehead = document.getElementsByClassName('forehead');
     
@@ -7,12 +9,18 @@ function blink(){
     var eyeLeft = document.getElementsByClassName('eyes--left');
     var eyeRight = document.getElementsByClassName('eyes--right');
 
+    var shoulder = document.getElementsByClassName('shoulder');
     var trunk = document.getElementsByClassName('trunk');
     var armsLeft = document.getElementsByClassName('arms--left');
     var armsRight = document.getElementsByClassName('arms--right');
 
+    var sectionBtn = document.getElementsByClassName('section-btn');
+
+
+    hair[0].classList.add('fringe');
 
     head[0].classList.add('skin--blink');
+    head[0].classList.add('head--blink');
     forehead[0].classList.add('skin--blink');
 
     
@@ -22,8 +30,11 @@ function blink(){
     eyeLeft[0].classList.add('eyes--blink');
     eyeRight[0].classList.add('eyes--blink');
 
+    shoulder[0].classList.add('shoulder--blink');
     trunk[0].classList.add('trunk--blink');
     armsLeft[0].classList.add('arms--left--blink');
     armsRight[0].classList.add('arms--right--blink');
+
+    sectionBtn[0].classList.add('section-btn--blink');
     
 }
