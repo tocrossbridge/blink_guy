29a515ed97e334d1c31048e07eada1ee5ca4ae54
Bea <beatriztattooer@gmail.com>
2021-02-28 14:05:37 -0300

    SKIN & SMILE OMG, he' smilinggggg!

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,19 @@
 function blink(){
+    var head = document.getElementsByClassName('head');
+    var forehead = document.getElementsByClassName('forehead');
+    
+    var mouth = document.getElementsByClassName('mouth');
+
     var eyeLeft = document.getElementsByClassName('eyes--left');
     var eyeRight = document.getElementsByClassName('eyes--right');
+
+
+    head[0].classList.add('skin--blink');
+    forehead[0].classList.add('skin--blink');
+
+    
+    mouth[0].classList.add('mouth--blink');
+    mouth[0].classList.remove('mouth');
 
     eyeLeft[0].classList.add('eyes--blink');
     eyeRight[0].classList.add('eyes--blink');
